ea23493d2b64bed8bffdb707159a33c8204afa2f
Hyman <1604723751@qq.com>
2016-01-15 20:52:27 +0800

    Add the basic structure of MVC feature

---
+++ SpringMvcApplication/Console/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Console.Startup))]
+namespace Console
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
